149add3fbb13871ed434d13032e941a131d353a8
Raúl Villares <raulvillares@gmail.com>
2022-02-08 23:24:34 +0100

    Set up 'replace constructors with creational methods' refactor

---
+++ RefactoringToPatterns/CreationMethods/ProductPackageShould.cs
@@ -0,0 +1,49 @@
+using System.Runtime.Remoting.Channels;
+using RefactoringToPatterns.CreationMethods.Tests;
+using Xunit;
+
+namespace RefactoringToPatterns.CreationMethods
+{
+    public class ProductPackageShould
+    {
+        [Fact]
+        public void CreateAProductPackageWithOnlyInternet()
+        {
+            var productPackage = new ProductPackage("100MB");
+
+            Assert.True(productPackage.HasInternet());
+            Assert.False(productPackage.HasVOIP());
+            Assert.False(productPackage.HasTv());
+        }
+
+        [Fact]
+        public void CreateWithInternetAndVOIP()
+        {
+            var productPackage = new ProductPackage("100MB", 91233788);
+
+            Assert.True(productPackage.HasInternet());
+            Assert.True(productPackage.HasVOIP());
+            Assert.False(productPackage.HasTv());
+        }
+
+        [Fact]
+        public void CreateWithInternetAndTv()
+        {
+            var productPackage = new ProductPackage("100MB", new string[] {"LaLiga", "Estrenos"});
+
+            Assert.True(productPackage.HasInternet());
+            Assert.False(productPackage.HasVOIP());
+            Assert.True(productPackage.HasTv());
+        }
+
+        [Fact]
+        public void CreateWithInternetVOIPAndTv()
+        {
+            var productPackage = new ProductPackage("100MB", 91233788, new string[] {"LaLiga", "Estrenos"});
+
+            Assert.True(productPackage.HasInternet());
+            Assert.True(productPackage.HasVOIP());
+            Assert.True(productPackage.HasTv());
+        }
+    }
+}
