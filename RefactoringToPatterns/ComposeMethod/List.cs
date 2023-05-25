bab365ea0f28ff980e186a23284f83fe5fa2eae2
Victor Garcia <vgarcia@aidacanarias.com>
2023-05-25 17:36:05 +0200

    Refactor: Extract ExtendList method

--- RefactoringToPatterns/ComposeMethod/List.cs
+++ RefactoringToPatterns/ComposeMethod/List.cs
@@ -22,17 +22,23 @@ namespace RefactoringToPatterns.ComposeMethod
             if(!_readOnly) {
                 int newSize = _size + 1;
 
-                if(newSize > _elements.Length) {
-                    Object[] newElements = new Object[_elements.Length + 10];
-
-                    for (int i = 0; i < _size; i++)
-                        newElements[i] = _elements[i];
-
-                    _elements = newElements;
+                if(newSize > _elements.Length)
+                {
+                    ExpandList();
                 }
 
                 AddElement(element);
             }
+        }
+
+        private void ExpandList()
+        {
+            Object[] newElements = new Object[_elements.Length + 10];
+
+            for (int i = 0; i < _size; i++)
+                newElements[i] = _elements[i];
+
+            _elements = newElements;
         }
 
         private void AddElement(object element)
